0b164999531d9876268a97f544c029cd6c9e8ca2
jgarciagonj97 <j.garciagonj@gmail.com>
2020-06-15 17:53:00 +0200

    Creación fichero de middlewares y ruta y modelo de logeados para validar mañana el token.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postRouter = require('./routes/posts');
 var blogRouter = require('./routes/blog');
+var { mainRedirect } = require('./routes/middlewares');
 
 //Para leer archivos .env
 require('dotenv').config();
@@ -17,12 +19,14 @@ var app = express();
 //Usar las variables/funciones exportadas de archivo db
 require('./db').conexion();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(mainRedirect);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postRouter);
