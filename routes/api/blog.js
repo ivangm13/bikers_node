cabc42b143b7ef325ba41a5e4d083255e17b8abf
jonhlop <jonathan.lopher@hotmail.com>
2020-06-16 10:13:39 +0200

    ok

--- routes/api/blog.js
+++ routes/api/blog.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-const Blog = require('../models/blog')
+const Blog = require('../../models/blog')
 /* GET users listing. */
-<<<<<<< HEAD:routes/blog.js
 
 router.get('/', (req, res) => {
-  Blog.getAllPosts()
-      .then((rows) => {
-          res.json(rows);
-      })
-      .catch(err => {
-          res.json({
-              error: err.message
-          });
-      });
-=======
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
->>>>>>> develop:routes/api/blog.js
-});
+    Blog.getAllPosts()
+        .then((rows) => {
+            res.json(rows);
+        })
+        .catch(err => {
+            res.json({
+                error: err.message
+            });
+        });
+})
 
 module.exports = router;
