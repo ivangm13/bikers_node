68147d8439224eccc18586a22314207ffdde4bbd
ivangm13 <ivangarcia346@gmail.com>
2020-06-18 16:49:59 +0200

    recuperar posts con fecha

--- routes/api/blog.js
+++ routes/api/blog.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
 
 const Blog = require('../../models/blog')
 /* GET users listing. */
@@ -16,14 +17,14 @@ router.get('/', (req, res) => {
         });
 })
 
-router.get('/:idBlog',async (req,res)=>{
-    try{
-      const result = await Blog.getBlogActivo(req.params.idBlog);
-    res.json(result)  
-    }catch(err){
+router.get('/:idBlog', async (req, res) => {
+    try {
+        const result = await Blog.getBlogActivo(req.params.idBlog);
+        result.fecha = moment(result.fecha).format('DD/MM/YYYY');
+        res.json(result);
+    } catch (err) {
         res.json(err);
     }
-    
 })
 
 module.exports = router;
