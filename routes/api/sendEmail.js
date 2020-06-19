3b1d02cd84b7fd720eccbd15f1654e89790505d5
jonhlop <jonathan.lopher@hotmail.com>
2020-06-19 15:28:06 +0200

    Merge branch 'develop' into featured-nodemailer
    # Conflicts:
    #	routes/api.js
    #	routes/api/send-email.js

---
+++ routes/api/sendEmail.js
@@ -0,0 +1,31 @@
+var express = require('express');
+const nodemailer = require('nodemailer')
+var router = express.Router();
+
+
+module.exports = (formulario) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'bikersaskus@gmail.com', // Cambialo por tu email
+            pass: 'neolan20' // Cambialo por tu password
+        }
+    });
+    const mailOptions = {
+        from: `”${formulario.nombre} ” <${formulario.email}>`,
+        to: 'bikersaskus@gmail.com', // Cambia esta parte por el destinatario
+        subject: formulario.asunto,
+        html: `
+ <h2>Nombre:</h2> ${formulario.nombre} <br/>
+ <strong>E-mail:</strong> ${formulario.email} <br/>
+ <p>${formulario.motivoConsulta}</p>
+ <strong>Mensaje:</strong> ${formulario.consulta}
+ `
+    };
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err)
+            console.log(err)
+        else
+            console.log(info);
+    });
+}
