355392c984c493130e07739be6034ca71a52d1fa
jgarciagonj97 <j.garciagonj@gmail.com>
2020-06-19 19:17:15 +0200

    Intento de subida de ficheros desde Angular. No puedo probar porque heroku no está levantado.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart();
 
 var apiRouter = require('./routes/api');
 var { mainRedirect } = require('./routes/middlewares');
@@ -22,9 +24,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(mainRedirect);
 app.use('/api', apiRouter);
 
+app.post('/', multipartMiddleware, (req, res) => {
+    console.log(req.body)
+    console.log(req.files.imagen.path)
+    let content = fs.readFileSync(req.files.imagen.path)
+    fs.writeFileSync('./images/test.png', content)
+    res.json({ success: "Todo correcto" })
+})
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
