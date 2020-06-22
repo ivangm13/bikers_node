654aee1a6b080713556b7830b39d73b0e08f4b8a
jgarciagonj97 <j.garciagonj@gmail.com>
2020-06-22 15:14:59 +0200

    Borrado código inútil app.js.

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-const multipart = require('connect-multiparty');
-const multipartMiddleware = multipart();
 
 var apiRouter = require('./routes/api');
 var { mainRedirect } = require('./routes/middlewares');
@@ -28,13 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mainRedirect);
 app.use('/api', apiRouter);
 
-app.post('/', multipartMiddleware, (req, res) => {
-    console.log(req.body)
-    console.log(req.files.imagen.path)
-    let content = fs.readFileSync(req.files.imagen.path)
-    fs.writeFileSync('./images/test.png', content)
-    res.json({ success: "Todo correcto" })
-})
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
