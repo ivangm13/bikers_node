860a7f60ee8db86e9c1dc61082c57910fc526f06
jgarciagonj97 <j.garciagonj@gmail.com>
2020-06-22 16:59:52 +0200

    Pruebas subida.

--- routes/api/blog.js
+++ routes/api/blog.js
@@ -28,14 +28,13 @@ router.get('/:idBlog', async (req, res) => {
     }
 });
 
-
 router.post('/create', async (req, res) => {
     console.log(req.body);
     try {
         const post = await Blog.crearBlog(req.body);
         res.json(post);
     } catch (err) {
-        res.json('Peticion fallida');
+        res.json(err);
     }
 });
 
